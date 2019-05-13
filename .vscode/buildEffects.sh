71910378ab011b2b61654116c314b9bf02fea9ab
toyboot4e <toyboot4e@gmail.com>
2019-05-14 01:37:58 +0900

    added VSCode/FNA settings
    provided by the VSCode-FNA-Template

---
+++ .vscode/buildEffects.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# buildEffects
+# Compiles all .fx files found in the project's Content directory.
+# Intended for usage with VS Code Build Tasks tooling.
+# You may need to change the path to fxc.exe depending on your installation.
+
+printf "Starting build process...\n"
+
+cd Rot.Game
+for file in `find ./Content/** -name "*.fx"` ;
+do
+    # Hush, wine...
+    export WINEDEBUG=fixme-all,err-all
+
+    # Build the effect
+    wine ~/.wine/drive_c/Program\ Files\ \(x86\)/Microsoft\ DirectX\ SDK\ \(June\ 2010\)/Utilities/bin/x86/fxc.exe\
+     /T fx_2_0 $file /Fo "`dirname $file`/`basename $file .fx`.fxb"
+
+    echo ""
+
+done
