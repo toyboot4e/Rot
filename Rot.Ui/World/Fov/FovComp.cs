82dc02cd09ca80779145ad1d6f0baeadff2e9bab
toyboot4e <toyboot4e@gmail.com>
2020-02-11 02:36:24 +0900

    rename ShadowCasting to Scanner

--- Rot.Ui/World/Fov/FovComp.cs
+++ Rot.Ui/World/Fov/FovComp.cs
@@ -33,7 +33,7 @@ namespace Rot.Ui {
         public void refresh() {
             var origin = this.Entity.get<Body>().pos;
             int radius = Preferences.fovRadius;
-            ShadowCasting<Fov, Stage>.refresh(this.fovFow, this.stage, origin.x, origin.y, radius);
+            Scanner<Fov, Stage>.refresh(this.fovFow, this.stage, origin.x, origin.y, radius);
             this.fovRenderer.onRefresh();
         }
 
