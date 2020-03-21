9588d2abbdc579a98cbf7b9a31992ce1cedcf02d
toyboot4e <toyboot4e@gmail.com>
2020-03-21 23:47:35 +0900

    refactor SpriteAnimatorT with internal struct

--- NezEp/Prelude/NezAdditions/SpriteAnimatorT.cs
+++ NezEp/Prelude/NezAdditions/SpriteAnimatorT.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using Nez;
 using Nez.Textures;
 
 namespace Nez.Sprites {
@@ -19,78 +18,42 @@ namespace Nez.Sprites {
             PingPongOnce
         }
 
-        public enum State {
-            None,
-            Running,
-            Paused,
-            Completed
+        public SpriteAnimatorT() { }
+        public SpriteAnimatorT(Sprite sprite) => base.SetSprite(sprite);
+
+        public struct Settings {
+            public Dictionary<T, SpriteAnimation> anims;
+            public Mode mode;
+            public float speed;
+
+            public Settings(Mode mode, float speed) {
+                this.anims = new Dictionary<T, SpriteAnimation>();
+                this.mode = mode;
+                this.speed = speed;
+            }
         }
 
+        Settings settings;
+        State state;
         public event Action<T> onComp;
-        public float speed = 1;
-        public State state { get; private set; } = State.None;
-        public SpriteAnimation currentAnim { get; private set; }
-        public T currentKey { get; private set; }
-        public int currentFrame { get; private set; }
-        public bool isRunning => state == State.Running;
-
-        readonly Dictionary<T, SpriteAnimation> _anims = new Dictionary<T, SpriteAnimation>();
-        float _elapsedTime;
-        Mode _loopMode;
-
-        public SpriteAnimatorT() { }
-        public SpriteAnimatorT(Sprite sprite) => SetSprite(sprite);
 
         void IUpdatable.Update() {
-            if (this.state != State.Running || this.currentAnim == null)
+            if (this.state.flag != State.Flag.Running || this.state.current.anim == null)
                 return;
 
-            var anim = this.currentAnim;
-            var secondsPerFrame = 1 / (anim.FrameRate * this.speed);
-            var iterationDuration = secondsPerFrame * anim.Sprites.Length;
-            var pingPongIterationDuration = anim.Sprites.Length < 3 ? iterationDuration : secondsPerFrame * (anim.Sprites.Length * 2 - 2);
-
-            _elapsedTime += Time.DeltaTime;
-            var time = Math.Abs(_elapsedTime);
-
-            // Once and PingPongOnce reset back to Time = 0 once they complete
-            if (_loopMode == Mode.Once && time > iterationDuration ||
-                _loopMode == Mode.PingPongOnce && time > pingPongIterationDuration) {
-                this.state = State.Completed;
-                this._elapsedTime = 0;
-                this.currentFrame = 0;
-                Sprite = anim.Sprites[0];
-                onComp?.Invoke(currentKey);
-                return;
+            var result = this.state.update(ref this.settings);
+            this.Sprite = this.state.current.anim.Sprites[result.frame];
+            if (result.isCompleted) {
+                this.onComp?.Invoke(this.state.current.key);
             }
-
-            if (_loopMode == Mode.ClampForever && time > iterationDuration) {
-                this.state = State.Completed;
-                this.currentFrame = anim.Sprites.Length - 1;
-                this.Sprite = anim.Sprites[currentFrame];
-                this.onComp?.Invoke(currentKey);
-                return;
-            }
-
-            // figure out which frame we are on
-            int i = Mathf.FloorToInt(time / secondsPerFrame);
-            int n = anim.Sprites.Length;
-            if (n > 2 && (_loopMode == Mode.PingPong || _loopMode == Mode.PingPongOnce)) {
-                // create a pingpong frame
-                int maxIndex = n - 1;
-                this.currentFrame = maxIndex - Math.Abs(maxIndex - i % (maxIndex * 2));
-            } else
-                // create a looping frame
-                this.currentFrame = i % n;
-
-            this.Sprite = anim.Sprites[currentFrame];
         }
 
         public SpriteAnimatorT<T> add(T key, SpriteAnimation animation) {
             // if we have no sprite use the first frame we find
-            if (Sprite == null && animation.Sprites.Length > 0)
-                SetSprite(animation.Sprites[0]);
-            _anims[key] = animation;
+            if (Sprite == null && animation.Sprites.Length > 0) {
+                base.SetSprite(animation.Sprites[0]);
+            }
+            this.settings.anims[key] = animation;
             return this;
         }
 
@@ -101,27 +64,114 @@ namespace Nez.Sprites {
         }
 
         #region Playback
-
-        public bool isActive(T key) => this.currentAnim != null && this.currentKey.Equals(key);
-        public void play(T key, Mode loopMode = Mode.Loop) {
-            this.currentAnim = _anims[key];
-            this.currentKey = key;
-            this.currentFrame = 0;
-            this.state = State.Running;
-
-            this.Sprite = currentAnim.Sprites[0];
-            this._elapsedTime = 0;
-            this._loopMode = loopMode;
+        public bool isActive(T key) => this.state.isActive(key);
+        public void play(T key, Mode mode = Mode.Loop) {
+            var anim = this.settings.anims[key];
+            this.state.play(key, anim);
+            this.Sprite = anim.Sprites[0];
         }
-        public void pause() => state = State.Paused;
-        public void resume() => state = State.Running;
-        public void stop() {
-            currentAnim = null;
-            currentKey = default(T);
-            currentFrame = 0;
-            state = State.None;
-        }
+        public void pause() => this.state.pause();
+        public void resume() => this.state.resume();
+        public void stop() => this.state.stop();
+        #endregion
 
+        #region internals
+        struct State {
+            public enum Flag {
+                None,
+                Running,
+                Paused,
+                Completed
+            }
+
+            public struct AnimData {
+                public SpriteAnimation anim;
+                public T key;
+                public int frame;
+            }
+
+            public float elapsed;
+            public AnimData current;
+            public Flag flag;
+
+            public State(AnimData current) {
+                this.elapsed = 0;
+                this.flag = Flag.None;
+                this.current = current;
+            }
+
+            public struct Result {
+                public bool isCompleted;
+                public int frame;
+            }
+
+            public Result update(ref Settings settings) {
+                var anim = this.current.anim;
+                var secondsPerFrame = 1 / (anim.FrameRate * settings.speed);
+                var iterationDuration = secondsPerFrame * anim.Sprites.Length;
+                var pingPongIterationDuration = anim.Sprites.Length < 3 ? iterationDuration : secondsPerFrame * (anim.Sprites.Length * 2 - 2);
+
+                this.elapsed += Time.DeltaTime;
+                var time = Math.Abs(this.elapsed);
+
+                // Once and PingPongOnce reset back to Time = 0 once they complete
+                if (settings.mode == Mode.Once && time > iterationDuration ||
+                    settings.mode == Mode.PingPongOnce && time > pingPongIterationDuration) {
+                    this.flag = Flag.Completed;
+                    this.elapsed = 0;
+                    this.current.frame = 0;
+
+                    return new Result() {
+                        isCompleted = true,
+                            frame = this.current.frame,
+                    };
+                }
+
+                if (settings.mode == Mode.ClampForever && time > iterationDuration) {
+                    this.flag = Flag.Completed;
+                    this.current.frame = anim.Sprites.Length - 1;
+                    // this.Sprite = anim.Sprites[this.current.frame];
+                    // this.onComp?.Invoke(currentKey);
+                    return new Result() {
+                        isCompleted = true,
+                            frame = this.current.frame,
+                    };
+                }
+
+                // figure out which frame we are on
+                int i = Mathf.FloorToInt(time / secondsPerFrame);
+                int n = anim.Sprites.Length;
+                if (n > 2 && (settings.mode == Mode.PingPong || settings.mode == Mode.PingPongOnce)) {
+                    // create a pingpong frame
+                    int maxIndex = n - 1;
+                    this.current.frame = maxIndex - Math.Abs(maxIndex - i % (maxIndex * 2));
+                } else
+                    // create a looping frame
+                    this.current.frame = i % n;
+
+                return new Result() {
+                    isCompleted = false,
+                        frame = this.current.frame,
+                };
+            }
+
+            public bool isActive(T key) => this.current.anim != null && this.current.key.Equals(key);
+            public void play(T key, SpriteAnimation anim) {
+                this.current.anim = anim;
+                this.current.key = key;
+                this.current.frame = 0;
+                this.flag = Flag.Running;
+                this.elapsed = 0;
+            }
+            public void pause() => this.flag = Flag.Paused;
+            public void resume() => this.flag = Flag.Running;
+            public void stop() {
+                this.current.anim = null;
+                this.current.key = default(T);
+                this.current.frame = 0;
+                this.flag = Flag.None;
+            }
+        }
         #endregion
     }
 }
